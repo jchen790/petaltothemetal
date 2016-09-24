67e4caa7682768cdabc1477a191fa3980a2efaf0
Jia-Uei Chen <Jia-Uei Chen>
2016-09-24 08:15:50 -0400

    cleaning code

--- blowscript.js
+++ blowscript.js
@@ -1,11 +1,3 @@
-let isMenuVisible = true;
-let isGridVisible = true;
-
-function toggle() {  
-    isMenuVisible = !isMenuVisible;
-    isGridVisible = !isMenuVisible;      
-}
-
 function toggle(e1, e2) {
     let x = document.getElementById(e1);
     let y = document.getElementById(e2);
