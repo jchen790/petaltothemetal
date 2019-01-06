db8ff02ffc9e721272d7edcbcaf5eaacf241c6dc
jchen790 <jchen790@gmail.com>
2019-01-06 14:40:22 -0800

    Updates

---
+++ precache-manifest.377b2622bbfdf60c2f834de0cffe2e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47dcfa5d4de7dd876996",
+    "url": "/petaltothemetal/static/css/main.ec5d21b5.chunk.css"
+  },
+  {
+    "revision": "47dcfa5d4de7dd876996",
+    "url": "/petaltothemetal/static/js/main.47dcfa5d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/petaltothemetal/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "6273ac915f0e927e71bf",
+    "url": "/petaltothemetal/static/js/runtime~main.6273ac91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/petaltothemetal/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b016ba3494ac899a866a79ce1e61dd06",
+    "url": "/petaltothemetal/index.html"
+  }
+];
